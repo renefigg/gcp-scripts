851e789fb8e15c6c077e412d199801672c4a1be8
nachirre <nachirre@gmail.com>
2021-08-02 16:59:39 -0400

    testing curl using jq and one data file

--- ab-scripts/curl-load-test.sh
+++ ab-scripts/curl-load-test.sh
@@ -2,7 +2,7 @@
 
 # Generate Test Data for ab
 if [ -z "$1" ]; then
-    echo -e "\n use: curl-load-test.sh number-requests concurrency delay"
+    echo -e "\n use: curl-load-test.sh number-requests concurrency delay url in-file"
     echo -e "\n"
     exit 1
 fi
@@ -10,19 +10,17 @@ fi
 CONCURRENCY=$2
 REQUESTS=$(( $(($1))-$(($CONCURRENCY)) ))
 DELAY=$3
+IN_FILE=$5
 r=0
 l=1
-while [ $r -le $REQUESTS ] 
+while [ $r -le $REQUESTS ]
 do
-    for c in $(eval echo "{1..$CONCURRENCY}")  
-    do 
+    for c in $(eval echo "{1..$CONCURRENCY}")
+    do
+	      PAYLOAD=$(jq '.['$r']' $IN_FILE)
         r=$(($r+1))
         echo "thread: $c.$l , request: $r"
-        curl -d @data/payload.$n.json -H 'Content-Type: application/json' \
-        -X POST \
-        localhost:3000/dispatcher/notify &
-
-
+        curl -d "$PAYLOAD" -H 'Content-Type: application/json' -X POST "$4" &
     done
     l=$(($l+1))
     sleep $DELAY
--- ab-scripts/generate-testdata.sh
+++ ab-scripts/generate-testdata.sh
@@ -2,11 +2,11 @@
 
 # Generate Test Data for ab
 if [ -z "$1" ]; then
-    echo -e "\n use: generate-testdata template tag begin end"
+    echo -e "\n use: generate-testdata template tag begin end prefix out-file"
     echo "template: template file with tags"
     echo "tag: tag to replace"
-    echo "begin: start number of ocurrencies" 
-    echo "end: end number of ocurrencies" 
+    echo "begin: start number of ocurrencies"
+    echo "end: end number of ocurrencies"
     echo "prefix: prefix character"
 
     echo -e "\n"
@@ -16,8 +16,12 @@ fi
 BEGIN=$3
 END=$4
 PREFIX=$5
-rm out.json 
-for i in $(eval echo "{$BEGIN..$END}") 
-   do cat $1 | sed "s/$2/$PREFIX$i/" >> out.json; 
+OUT_FILE=$6
+echo "[" > $OUT_FILE
+for i in $(eval echo "{$BEGIN..$END}")
+   do cat $1 | sed "s/$2/$PREFIX$i/" >> $OUT_FILE;
+      if [ $i -ne $END ]; then
+        echo "," >> $OUT_FILE;
+      fi
 done
- 
+echo "]" >> $OUT_FILE
