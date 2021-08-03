23535fb16d7abfd24cb2ce81cdff185af571058a
nachirre <nachirre@gmail.com>
2021-08-03 19:07:04 -0400

    generating data in the curl script

--- ab-scripts/curl-load-multiple.sh
+++ ab-scripts/curl-load-multiple.sh
@@ -2,23 +2,34 @@
 
 # Generate Test Data for ab
 if [ -z "$1" ]; then
-    echo -e "\n use: curl-load-test.sh number-requests concurrency delay url"
+    echo -e "\n use: curl-load-test.sh begin end prefix  concurrency delay url"
     echo -e "\n"
     exit 1
 fi
 
-CONCURRENCY=$2
-REQUESTS=$(( $(($1))-$(($CONCURRENCY)) ))
-DELAY=$3
-r=0
+LABEL=%NUMBER%
+TEMPLATE=t1.json
+BEGIN=$1
+END=$2
+PREFIX=$3
+rm -f data/*
+for i in $(eval echo "{$BEGIN..$END}")
+   do cat $TEMPLATE | sed "s/$LABEL/$PREFIX$i/" > data/payload.$i.json;
+done
+
+CONCURRENCY=$4
+REQUESTS=$(( $(($END - $BEGIN + 1))-$(($CONCURRENCY)) ))
+DELAY=$5
+URL=$6
+r=$(($BEGIN - 1))
 l=1
-while [ $r -le $REQUESTS ]
+while [ $r -le $(($END - $CONCURRENCY)) ]
 do
     for c in $(eval echo "{1..$CONCURRENCY}")
     do
         r=$(($r+1))
-        echo "thread: $c.$l , request: $r"
-        curl -d @data/payload.$r.json -H 'Content-Type: application/json' -X POST "$4" &
+        echo -n -e "thread: $c.$l , request: $r\r"
+        curl -s -d @data/payload.$r.json -H 'Content-Type: application/json' -X POST "$6" > data/out.$r.log &
     done
     l=$(($l+1))
     sleep $DELAY
