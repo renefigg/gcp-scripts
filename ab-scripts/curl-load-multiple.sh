15f1e45e7b355f435406c4fccf9db158bca9f80c
nachirre <nachirre@gmail.com>
2021-08-03 19:17:03 -0400

    changes merged

--- ab-scripts/curl-load-multiple.sh
+++ ab-scripts/curl-load-multiple.sh
@@ -7,7 +7,6 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-<<<<<<< HEAD
 LABEL=%NUMBER%
 TEMPLATE=t1.json
 BEGIN=$1
@@ -25,28 +24,12 @@ URL=$6
 r=$(($BEGIN - 1))
 l=1
 while [ $r -le $(($END - $CONCURRENCY)) ]
-=======
-CONCURRENCY=$2
-REQUESTS=$1
-DELAY=$3
-r=0
-l=1
-rm -Rf output
-mkdir output
-while [ $r -le $REQUESTS ]
->>>>>>> badce0305ffa2e921b75e9b3568a15ca288ddc04
 do
     for c in $(eval echo "{1..$CONCURRENCY}")
     do
         r=$(($r+1))
-<<<<<<< HEAD
         echo -n -e "thread: $c.$l , request: $r\r"
         curl -s -d @data/payload.$r.json -H 'Content-Type: application/json' -X POST "$6" > data/out.$r.log &
-=======
-        echo -n  -e "thread:$l , request: $r/$REQUESTS \r" 
-        curl localhost > output/result.$r.html & 
-#       curl -d @data/payload.$r.json -H 'Content-Type: application/json' -X POST "$4" &
->>>>>>> badce0305ffa2e921b75e9b3568a15ca288ddc04
     done
     l=$(($l+1))
     sleep $DELAY
